3835c70ac098ef77ed2fdaab85cb25614594299c
gracege29 <gracege29@163.com>
2019-10-19 17:41:28 +0800

    add searchcolumn.js

---
+++ assignment05/searchcolumn.js
@@ -0,0 +1,24 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+$(document).ready(function() {
+    $('#data-table').DataTable( {
+        initComplete: function () {
+            var api = this.api();
+            api.columns().indexes().flatten().each( function ( i ) {
+                var column = api.column( i );
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }
+    } );
+} );
