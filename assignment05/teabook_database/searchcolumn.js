b121fac1004f253f9d28c74aef59e3ece463836c
gracege29 <gracege29@163.com>
2019-10-20 11:35:32 +0800

    use searchcolumn.js

--- assignment05/teabook_database/searchcolumn.js
+++ assignment05/teabook_database/searchcolumn.js
@@ -2,19 +2,20 @@
 $(document).ready(function() {
     $('#data-table').DataTable( {
         initComplete: function () {
-            var api = this.api();
-            api.columns().indexes().flatten().each( function ( i ) {
-                var column = api.column( i );
+            this.api().columns().every( function () {
+                var column = this;
                 var select = $('<select><option value=""></option></select>')
                     .appendTo( $(column.footer()).empty() )
                     .on( 'change', function () {
                         var val = $.fn.dataTable.util.escapeRegex(
                             $(this).val()
                         );
+ 
                         column
                             .search( val ? '^'+val+'$' : '', true, false )
                             .draw();
                     } );
+ 
                 column.data().unique().sort().each( function ( d, j ) {
                     select.append( '<option value="'+d+'">'+d+'</option>' )
                 } );
