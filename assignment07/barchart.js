296c9a8b53922d36cf08104edb24d95a75df4121
gracege29 <gracege29@163.com>
2019-11-01 18:24:07 +0800

    Update barchart.js

--- assignment07/barchart.js
+++ assignment07/barchart.js
@@ -38,8 +38,10 @@ $(document).ready(function(){
                     type:'bar'
                     },
                     axis:{
-                    x:{type: 'category',
-                    categories: ['人均价格','饮品数量','店铺数量']},
+                            x:{
+                                type: 'category',
+                                categories: ['人均价格','饮品数量','店铺数量']
+                            }
                     },
                     
                     bar: {
