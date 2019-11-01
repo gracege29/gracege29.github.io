78b7d97f1b26342f5f39c71749f54c167eb6ebbe
gracege29 <gracege29@163.com>
2019-11-01 17:27:44 +0800

    add bar chart

---
+++ assignment07/barchart.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    $("button#get_barchart").click(function() {
+        var items = [];
+        var i = 0;
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appWHHOrncvf1qw2X/Table%201?api_key=keyVPDmCemvZStnmu";
+        var dataSet = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+            $.each(result.records, function(key,value) {
+                   items = [];
+                       items.push(value.fields.Name);
+                       items.push(value.fields.average_price);
+                       items.push(value.fields.number_of_drinks);
+                       items.push(value.fields.number_of_shops);
+                       dataSet.push(items);
+                       console.log(items);
+                }); // end .each
+                console.log(dataSet);
+
+                $('#table1').DataTable({
+                    data: dataSet,
+                    retrieve: true,
+                    columns: [
+                        { title: "店铺名称",
+                          defaultContent:""},
+                        { title: "人均价格",
+                            defaultContent:"" },
+                        { title: "饮品数量",
+                          defaultContent:"" },
+                        { title: "店铺数量",
+                          defaultContent:"" },
+                    ]
+                });
+
+                var chart = c3.generate({
+                    data:{
+                    columns:dataset,
+                    type:'bar'
+                    },
+                    axis:{
+                    x:{label:'店铺名称'},
+                    y:{label:'人均价格'}
+                    },
+                    
+                    bar: {
+                    width: {ratio: 0.5}
+                    }
+                });//end c3
+        });
+    });
+});
