5f618f828ddf1b6567cd803408ca18865fa9e2eb
gracege29 <gracege29@163.com>
2019-11-26 16:22:39 +0800

    Update barchart.js

--- exercise/BTA/barchart.js
+++ exercise/BTA/barchart.js
@@ -85,7 +85,7 @@ $(document).ready(function(){
             "<div id='fullScreen'></div>"+
             //浮层区
             "<div id='floatLayer'>" +
-                "<a href='#' id='hide' style='float:right'>关闭 </a>"+
+                "<a href='#manghe' id='hide' style='float:right'>关闭 </a>"+
                 "<iframe class='airtable-embed' src='https://airtable.com/embed/shr76XdaN525nvktm?backgroundColor=gray&viewControls=on' frameborder='0' onmousewheel='' width='100%' height='533' style='background: transparent; border: 1px solid #ccc;'></iframe>"+
             "</div>"
         );
